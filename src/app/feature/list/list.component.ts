15578eb4f7492dad8058bff3722c4df1e99fb9b0
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2025-11-01 19:13:56 -0300

    fix: refact input card

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -1,13 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../model/productData';
-import { MatCardModule } from "@angular/material/card"
-import { MatButtonModule } from '@angular/material/button';
+import { CardComponent } from '../coponents/card/card.component';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [MatCardModule, MatButtonModule],
+  imports: [CardComponent],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
