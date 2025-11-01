2e9b23ef6c637b406e41927b134e1378df40448a
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2025-11-01 19:26:49 -0300

    feat: add link create product

--- src/app/feature/list/list.component.ts
+++ src/app/feature/list/list.component.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { ProductService } from '../../services/product.service';
 import { Product } from '../../model/productData';
 import { CardComponent } from '../coponents/card/card.component';
+import { RouterLink } from "@angular/router";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, MatButtonModule, RouterLink],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
