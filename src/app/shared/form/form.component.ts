066f8ca54bda562066f7e7c0af8e6d2ddcd1d6c1
JamissonBarbosa <jamissondasilvafilho@gmail.com>
2025-12-13 18:53:12 -0300

    feat: create button backpage

--- src/app/shared/form/form.component.ts
+++ src/app/shared/form/form.component.ts
@@ -4,11 +4,12 @@ import { Product } from '../../model/productData';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatInputModule, MatButtonModule, RouterLink],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
